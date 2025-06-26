db6a34b22688d17157e4f3a78905909f16627989
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-06-26 10:02:55 +0600

    Create A_False_Alarm.cpp

---
+++ Code_Forces/Implementation/A_False_Alarm.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define nline "\n"
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define ff first
+#define ss second
+
+void solve()
+{
+    int n, x;
+    cin >> n >> x;
+    bool a[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (a[i])
+        {
+            for (int j = i + x; j <= n; j++)
+            {
+                if (a[j])
+                {
+                    No;
+                    return;
+                }
+            }
+        }
+    }
+    Yes;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
