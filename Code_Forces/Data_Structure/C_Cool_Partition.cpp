b61314d3b3c64cba54e0cfeda75df288c739ca7d
Siam Hossain <152617928+hossainsiam133@users.noreply.github.com>
2025-06-27 12:32:41 +0600

    Create C_Cool_Partition.cpp

---
+++ Code_Forces/Data_Structure/C_Cool_Partition.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define nline '\n'
+#define all(a) (a).begin(), (a).end()
+#define mid(l, r) ((r + l) / 2)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int ans = 1, ind = 0;
+    set<int> st;
+    st.insert(a[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (st.find(a[i]) != st.end())
+            st.erase(a[i]);
+        if (st.empty())
+        {
+            ans++;
+            for (int j = ind + 1; j <= i; j++)
+            {
+                st.insert(a[j]);
+            }
+            ind = i;
+        }
+    }
+    cout << ans << nline;
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
