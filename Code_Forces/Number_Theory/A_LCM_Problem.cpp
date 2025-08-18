5e66eb4dc07add243958fefa8dc3de629ca5369b
hossainsiam133 <hsiam133946@gmail.com>
2025-08-18 23:35:43 +0600

    Updated Commit

---
+++ Code_Forces/Number_Theory/A_LCM_Problem.cpp
@@ -0,0 +1,38 @@
+// https://codeforces.com/contest/1389/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define nline "\n"
+#define Yes cout << "YES\n"
+#define No cout << "NO\n"
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define ff first
+#define ss second
+
+void solve()
+{
+    int l, r;
+    cin >> l >> r;
+    if (l * 2 <= r)
+        cout << l << ' ' << l * 2 << nline;
+    else
+        cout << -1 << ' ' << -1 << nline;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
