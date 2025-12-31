dbe9518d9947195a63df5ea373d33b545281029b
hossainsiam133 <152617928+hossainsiam133@users.noreply.github.com>
2025-12-31 08:04:15 +0600

    Solved 2nd easiest problem of icpc 25 dhk regional and get wa1 for giving extra space...lol

---
+++ ICPC 2025/Regional/F_Morning_Walk.cpp
@@ -0,0 +1,66 @@
+// https://codeforces.com/gym/106270/problem/F
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define nline '\n'
+#define all(a) (a).begin(), (a).end()
+#define mid(l, r) ((r + l) / 2)
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+void solve()
+{
+    ll l, v, t1, q;
+    cin >> l >> v >> t1 >> q;
+    for (int i = 0; i < q; i++)
+    {
+        ll u, t2;
+        cin >> t2 >> u;
+        ll ans = (t1 - t2) / (l * 1.0 / abs(v - u));
+        cout << ans + 1 << " \n"[i + 1 == q];
+    }
+}
+
+int main()
+{
+    optimize();
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
